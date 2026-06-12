e5af2f35e97179e7fd7d2426c637f66797077bcf
morochrislegal <christian.moro@escola.pr.gov.br>
2026-06-12 09:29:24 -0300

    Add files via upload

---
+++ script/jogos.js
@@ -0,0 +1,85 @@
+const jogos = [{
+    titulo: "Minecraft",
+    imagem: "https://preview.redd.it/whats-the-name-of-this-texture-pack-from-the-games-cover-v0-4lv9qrcjjui71.jpg?width=640&crop=smart&auto=webp&s=ef037b75b942484e33bec8eb703e4fcf67da4fb6",
+    descricao: "O Minecraft é um jogo eletrônico de aventura do tipo sandbox onde os jogadores exploram um mundo aberto tridimensional feito de blocos",
+},
+{
+    titulo: "Roblox",
+    imagem: "https://newzoo.com/wp-content/uploads/api/games/artworks/game--roblox.jpg",
+    descricao: "O Roblox é uma plataforma imersiva global de jogos e criação",
+},
+{
+    titulo: "Dead By Daylight",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSomiTc-P-2UzgUdxEBAY4hD5jD49DTCZtZwQ&s",
+    descricao: "é um jogo de terror multijogador onde 4 Sobreviventes tentam fugir de 1 Assassino."
+},
+{
+    titulo: "Ultrakill",
+    imagem: "https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Ultrakill_cover.png/250px-Ultrakill_cover.png",
+    descricao: "é um jogo de tiro em primeira pessoa (FPS) rápido e violento. Você controla V1, um robô que desce ao Inferno em busca de sangue para sobreviver.",
+},
+{
+    titulo: "Grand Theft Auto 5",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/8/80/Grand_Theft_Auto_V_capa.png/250px-Grand_Theft_Auto_V_capa.png",
+    descricao: "Eles realizam assaltos perigosos em Los Santos para resolver problemas com o governo e máfias.",
+},
+{
+    titulo: "Fortnite",
+    imagem: "https://m.media-amazon.com/images/M/MV5BMTZlMmIxM2EtN2Y4Zi00M2ZhLTk3NzgtNjJmZTU0MTQ3YjcwXkEyXkFqcGc@._V1_.jpg",
+    descricao: " é um jogo online multijogador e uma plataforma de entretenimento. A sua modalidade mais famosa é o Battle Royale.",
+},
+{
+    titulo: "Rocket League",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnCawmBYMLpQBznASj8gVr87dN_2ppXbyZcg&s",
+    descricao: "é um jogo competitivo que mistura futebol, caos automobilístico e física realista.",
+},
+{
+    titulo: "Alien Isolation",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2XY_1L18M7bsrj7meNHfHjLMsWzYx1P6_LQ&s",
+    descricao: "é um jogo de terror e sobrevivência em primeira pessoa onde você controla Amanda Ripley, filha de Ellen Ripley (do filme clássico). Presa em uma estação espacial abandonada, você deve fugir de um único Xenomorfo implacável.",
+},
+{
+    titulo: "Resident Evil 7: Biohazard",
+    imagem: "https://store-images.s-microsoft.com/image/apps.38644.71433286839923669.1250138a-286b-4b3f-8367-3d201ae45ae9.a08b2cbe-68eb-4c9e-a196-78097193172f",
+    descricao: "é um jogo de terror e sobrevivência que se passa na primeira pessoa. Você controla Ethan Winters. Ele procura a sua esposa desaparecida há três anos, Mia."
+},
+{
+    titulo: "Resident Evil 4",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwOdn-mat1iV5T4HNZYoWi778Fo08Fm2Hi4Q&s",
+    descricao: "é um jogo de terror e ação onde você controla o agente Leon S. Kennedy. Sua missão é salvar Ashley, a filha do presidente dos Estados Unidos.",
+},
+]
+
+
+const listaJogos = document.getElementById("listaJogos");
+
+function mostrarJogos(lista) {
+    listaJogos.innerHTML = "";
+
+    lista.forEach(jogos => {
+        listaJogos.innerHTML += `
+        <div class="col-md-4">
+            <div class="card card-serie h-100">
+                <img src="${jogos.imagem}" class="card-img-top">
+                <div class="card-body">
+                    <h3>${jogos.titulo}</h3>
+                    <p>${jogos.descricao}</p>
+                    <button class="btn btn-dark" onclick="verDetalhes('${jogos.titulo}')">
+                        Ver detalhes
+                    </button>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+}
+
+mostrarJogos(jogos);
+
+function verDetalhes(titulo) {
+    Swal.fire({
+        title: "titulo",
+        text: "Mais informações da série",
+        icon: "info"
+    });
+}
---
+++ script/script.js
@@ -0,0 +1,75 @@
+const filmes = [{
+    titulo: "Gente Grande",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf3vk6keGwuFr3Ck1TNlt8nrXjSc58WK6xwQ&s",
+    descricao: "O filme acompanha cinco amigos de infância que se reencontram após trinta anos. Eles alugam uma casa no lago para passar o fim de semana do feriado de 4 de julho. ",
+},
+{
+    titulo: "Batman",
+    imagem:"https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    descricao: "O Cavaleiro das Trevas",
+},
+{
+    titulo: "Homem-Aranha",
+    imagem: "https://image.tmdb.org/t/p/w500/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg",
+    descricao: "O amigo da vizinhança",
+},
+
+{
+    titulo: "Minecraft",
+    imagem: "https://media.fstatic.com/N7WeSh0LNFtoL5cjCTmtk26k-i0=/full-fit-in/629x300/media/articles/main/2025/04/06/ff7ec16d-8161-4a24-bf4c-c64ad5a08591-596230497.jpg",
+    descricao: "O mundo quadrado",
+},
+{
+    titulo: "Os Devoradores de Estrela",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnMGavLRCvsiwbh7G2sg1heXElB1McGIeBZQ&s",
+    descricao: "Comendo as Estrela",
+},
+{
+    titulo: "Michael Jackson",
+    imagem: "https://www.estadao.com.br/resizer/v2/JJEYVX3G3RA3VF7RCQHMWEUWPE.jpg?quality=80&auth=88cc49b168b2aa57b596f9274a0e6c35225e769eeeecbdf22e98e6f9e8cc44dd&width=380",
+    descricao: "O Rei do Pop",
+},
+{
+    titulo: "Carros o Filme",
+    imagem: "https://cinemaweb.com.br/wp-content/uploads/2024/10/carros-4-vai-acontecer.jpg",
+    descricao: "Vrummm Vrummm Vrummmmmmmmm",
+},
+{
+    titulo: "",
+    imagem: "https://media.tenor.com/0Ql9ECzdfScAAAAM/no.gif",
+    descricao: "nonono",
+},
+]
+
+
+const listaFilmes = document.getElementById ("listaFilmes");
+
+function mostrarFilmes(lista) {
+    listaFilmes.innerHTML = ""
+    lista.forEach(filme => {
+        listaFilmes.innerHTML += `
+        <div class="col-md-4">
+            <div class="card card-filme h-100">
+            <img src="${filme.imagem}" class="card-img-top">
+            <div class="card-body">
+            <h3>${filme.titulo}</h3>
+            <p>${filme.descricao}</p>
+            <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes</button>
+        </div>
+        </div>
+    </div>
+        `
+    })
+}
+
+mostrarFilmes(filmes)
+function verDetalhes(titulo) {
+    Swal.fire({
+        title: "titulo",
+        text: "Mais informações do filme",
+        icon: "info"
+    }) 
+}
+
+
+
